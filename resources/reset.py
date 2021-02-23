6f80cc152db669b0da1b65130b51529a31f8412b
subas shrestha <subasshrestha513@gmail.com>
2021-02-23 21:32:28 +0545

    user model not removed while reset

--- resources/reset.py
+++ resources/reset.py
@@ -2,7 +2,7 @@ import os
 from flask_restful import Resource
 import shutil
 from database.db import db
-
+from models.child import ChildModel
 def remove_file(path):
     try:
         os.remove(path)
@@ -20,12 +20,12 @@ def remove_directory(path):
 class Reset(Resource):
     def get(self):
         remove_file('data.csv')
-        remove_file('data.db')
         remove_file('svm_model.sav')
         remove_directory('train')
         remove_directory('croped_images')
         remove_directory('images')
         remove_directory('temp_croped_images')
         remove_directory('temp_images')
-        db.create_all()
+        db.session.query(ChildModel).delete()
+        db.session.commit()
         return {"msg": "success"}, 200
